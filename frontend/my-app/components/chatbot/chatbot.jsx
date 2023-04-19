479371d10b8e85d0d720bade86ee648922f29707
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-04-19 12:12:15 +0530

    added user message inputs

--- frontend/my-app/components/chatbot/chatbot.jsx
+++ frontend/my-app/components/chatbot/chatbot.jsx
@@ -1,15 +1,38 @@
 import { createSignal } from "solid-js"
+import { createStore } from "solid-js/store"
 import "./chatbot.css"
 
 export default function ChatBot(props){
-    const [chatMessage,setChatMessage ] = createSignal(["Hi, I'm Jeevan Chat-Bot. How can I help you today?"])
+    const [chatMessages,setChatMessages ] = createStore([{
+        role:"assistant",message:"Hi, I'm Jeevan Chat-Bot. How can I help you today?"}])
+    const [userMessage, setUserMessage] = createSignal("")
+
+    function submitChatMessage(){
+        setChatMessages([...chatMessages,{role:"user",message:userMessage()}])
+        //console.log(chatMessages.role)
+    }
 
     return(
         <div className="chat-box">
             <div className="chat-message-area">
-                {chatMessage()}
+                <For each = {chatMessages}>
+                    { (chatMessage) =>{
+                        const {role,message} = chatMessage;
+                        return (
+                            <div> 
+                                <h4>{role }</h4>
+                                <p>{message}</p>
+                            </div>
+                        )
+                    }
+
+                    }
+                </For>
             </div>
-            <input className="user-input" type="text" placeholder="Enter your text" onSubmit={setChatMessage(chatMessage().push())}></input>
+            <div className="input-bar">
+                <input className="user-input" type="text" placeholder="Enter your text" onInput={(e)=>setUserMessage(e.target.value)}></input>
+                <button className="input-submit-btn" onClick={submitChatMessage}>Submit</button>
+            </div>
         </div>
     )
 }
