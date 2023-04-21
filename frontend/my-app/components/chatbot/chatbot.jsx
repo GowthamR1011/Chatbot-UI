dd23672d485f0f09f2f837ba146a2480bf89f095
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-04-21 12:21:24 +0530

    Fixed various bugs on chat and new chat featiues. Completed first build

--- frontend/my-app/components/chatbot/chatbot.jsx
+++ frontend/my-app/components/chatbot/chatbot.jsx
@@ -6,33 +6,86 @@ import AssistantChat from "./assistantchat/assistantchat"
 
 export default function ChatBot(props){
     const [chatMessages,setChatMessages ] = createStore([{
-        role:"assistant",message:"Hi, I'm Jeevan Chat-Bot. How can I help you today?"}])
+        role:"assistant",content:"Hi, I'm Jeevan Chat-Bot. How can I help you today?"}])
     const [userMessage, setUserMessage] = createSignal("")
     const [replyLoading,setReplyLoading] = createSignal(false)
+    var queryId="",prompt="",topic=[];
+        
+
     function submitChatMessage(){
-        setChatMessages([...chatMessages,{role:"user",message:userMessage()}])
+        setChatMessages([...chatMessages,{role:"user",content:userMessage()}])
         setReplyLoading(true);
-        //console.log(chatMessages.role)
-        setTimeout(getReply,1000);
+        setUserMessage("");
+        let messageHistory = getMessageHistory()
+
+        if(prompt="")
+        {
+            getFirstReply(messageHistory)
+        }
+        else{
+            getReply(messageHistory)
+        }
     }
 
-    function getReply(){
+
+    function getMessageHistory(){
+        if (chatMessages.length > 10)
+            return chatMessages.slice(-10)
+        
+        else
+            return chatMessages
+    }
+
+    async function getFirstReply(chatContent){
+        fetch("/api/newchat",{
+            method:"POST",
+            headers:{
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body:JSON.stringify({
+                messageHistory:chatContent
+            })
+        })
+
+        .then(res => res.json())
+        .then(replyObj => {
+            console.log(replyObj.reply)
+            prompt = replyObj.prompt;
+            topic = replyObj.topic;
+            setChatMessages([...chatMessages,replyObj.reply]);
+           setReplyLoading(false);
+        })
+        .catch(err =>{
+            console.log("Error in Getting message from server",err);
+        })
+    }
+
+
+    async function getReply(chatContent){
+        
+        
         fetch("/api/chat",{
             method:"POST",
             body:JSON.stringify({
-                queryid:123,
-                messageHistory:chatMessages
+                queryId:queryId,
+                messageHistory:chatContent,
+                prompt:prompt,
+                topic:topic 
             }),
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
             }
         })
-            .then((response) => {
-               setChatMessages([...chatMessages,{role:"assistant",message:"Great! Thats Good"}]);
+            .then((response) => response.json())
+            .then(chatreply => {
+                console.log(chatreply.reply);
+                prompt = chatreply.prompt;
+                topic = chatreply.topic;
+                setChatMessages([...chatMessages,chatreply.reply]);
                setReplyLoading(false);
             })
             .catch((err)=>{
-                console.log("Error in Fetch")
+                console.log("Error in Getting Reply from Server for multiple messages",err)
             })
     }
 
@@ -41,16 +94,16 @@ export default function ChatBot(props){
             <div className="chat-message-area">
                 <For each = {chatMessages}>
                     { (chatMessage) =>{
-                        const {role,message} = chatMessage;
+                        const {role,content} = chatMessage;
                         if (role == "user")
                         {
                             return (
-                               <UserChat  role={role} message={message}/>
+                               <UserChat  role={role} message={content}/>
                             )
                         }
                         else{
                             return(
-                                <AssistantChat message={message} />
+                                <AssistantChat message={content} />
                             )
                         }
                     }
@@ -62,7 +115,7 @@ export default function ChatBot(props){
                 </Show>
             </div>
             <div className="input-bar">
-                <input className="user-input" type="text" placeholder="Enter your text" onInput={(e)=>setUserMessage(e.target.value)}></input>
+                <input className="user-input" type="text" placeholder="Enter your text" onInput={(e)=>setUserMessage(e.target.value)} value={userMessage()}></input>
                 <button className="input-submit-btn" onClick={submitChatMessage}>Submit</button>
             </div>
         </div>
--- server/index.js
+++ server/index.js
@@ -2,25 +2,78 @@ var express = require("express");
 var path = require("path")
 var app = express();
 var bodyParser = require('body-parser');
-var chat = require("./chat/chatbot")
 
-//var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 app.use(express.static(path.join(__dirname,"..","frontend/my-app/dist")))
 app.use(express.urlencoded({extended : true}))
 app.use(bodyParser.json());
 
 app.get("/home",function(req,res){
-    //console.log("Request from ",req.headers.origin)
+
     res.send("Welcome to Home Page")
 });
 
 
-app.post("/api/chat", function(req,res){
-    if(req.body.messageHistory){
-        messageHistory = chat.chatMessageProcessor(req.body.messageHistory)
-    }
+app.post("/api/newchat",function(req,res){
+    console.log("New Chat API called")
+    queryId = "abc"
+
+    fetch("http://127.0.0.1:8000/newchat",{
+        method:"POST",
+        body:JSON.stringify({
+            queryId:queryId,
+            messageHistory:req.body.messageHistory
+        }),
+        headers:{
+            "Content-type":"application/json; chatset=UTF-8"
+        }
+    })
     
-    res.sendStatus(200)
+    .then((reply) => reply.json())
+    .then((newMessage) => res.send(newMessage))
+
+    .catch(err => {
+        console.log(err)
+        res.send("Server Error",err)
+    })
+
+})
+
+
+
+app.post("/api/chat", function(req,res){
+
+        console.log("Request with Query ID: ",req.body.queryId);
+
+        fetch("http://127.0.0.1:8000/chat",{
+            method:"POST",
+            body:JSON.stringify({
+                queryId:"abc",
+                messageHistory:req.body.messageHistory,
+                prompt:req.body.prompt,
+                topic:req.body.topic
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+
+        })
+
+        .then((chatResp) => chatResp.json())
+        .then((newMessage)=> {
+            res.send(JSON.stringify({
+                queryId:req.body.queryId,
+                reply:newMessage.reply,
+                prompt:newMessage.prompt,
+                topic:newMessage.topic
+            }))    
+        })
+        .catch(err => {
+            console.log(err)
+            res.send("Server Error",err)
+        })
+
 })
 
 app.listen(5000,()=>{
